74fa3ac67f3393d9a77bae4aea6d4dbbeb6a6d0a
gilangrilhami <gilangrilhami@hotmail.co.uk>
2025-10-12 13:29:46 +0000

    feat: defined `POST /signals` endpoint to submit signals
    Signed-off-by: gilangrilhami <gilangrilhami@hotmail.co.uk>

--- src/routes/signals.py
+++ src/routes/signals.py
@@ -2,3 +2,7 @@ from fastapi import APIRouter
 
 signals = APIRouter(prefix="signals/", tags=["Signal"])
 
+
+@signals.post("")
+async def submit_signal(signal: dict):
+    return {"message": "Signal submitted successfully", "signal": signal}
